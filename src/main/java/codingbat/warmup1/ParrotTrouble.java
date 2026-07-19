1bd431aa3e5c3c6cb15832b63eda9a39dd9f0cf8
Alla <allabreus@gmail.com>
2026-07-20 01:42:03 +0300

    ParrotTrouble

--- src/main/java/codingbat/warmup1/ParrotTrouble.java
+++ src/main/java/codingbat/warmup1/ParrotTrouble.java
@@ -1,5 +1,11 @@
 package codingbat.warmup1;
 
 public class ParrotTrouble {
-
+    public boolean parrotTrouble(boolean talking, int hour) {
+        if (talking && (hour < 7 || hour > 20)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
