d4b02601d1b674889ea7717ccb442c583bb5b977
Alla <allabreus@gmail.com>
2026-07-19 23:01:51 +0300

    SeeColor

---
+++ src/main/java/codingbat/string1/SeeColor.java
@@ -0,0 +1,12 @@
+package codingbat.string1;
+
+public class SeeColor {
+    public String seeColor(String str) {
+        if (str.startsWith("red")) {
+            return "red";
+        } else if (str.startsWith("blue")) {
+            return "blue";
+        }
+        return "";
+    }
+}
