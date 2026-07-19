47fca1a1525233ccd43d63063498095acb6c6402
Alla <allabreus@gmail.com>
2026-07-19 23:42:10 +0300

    ConCat

---
+++ src/main/java/codingbat/string1/ConCat.java
@@ -0,0 +1,10 @@
+package codingbat.string1;
+
+public class ConCat {
+    public String conCat(String a, String b) {
+        if (a.length() > 0 && b.length() > 0 && a.charAt(a.length()-1) == b.charAt(0)) {
+            return a.substring(0,a.length()-1) + b;
+        }
+        return a + b;
+    }
+}
