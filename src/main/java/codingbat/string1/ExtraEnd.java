067f9aee42ce3e020c05cdf5230bd0d99825de16
Alla <allabreus@gmail.com>
2026-07-19 23:18:15 +0300

    ExtraEnd

---
+++ src/main/java/codingbat/string1/ExtraEnd.java
@@ -0,0 +1,8 @@
+package codingbat.string1;
+
+public class ExtraEnd {
+    public String extraEnd(String str) {
+        String word = str.substring(str.length()-2);
+        return word + word + word;
+    }
+}
