ccfebab242a9e8202eb19892a9a38d9126f8129c
Alla <allabreus@gmail.com>
2026-07-20 00:33:59 +0300

    Right2

---
+++ src/main/java/codingbat/string1/Right2.java
@@ -0,0 +1,7 @@
+package codingbat.string1;
+
+public class Right2 {
+    public String right2(String str) {
+        return str.substring(str.length()-2).concat(str.substring(0,str.length()-2));
+    }
+}
